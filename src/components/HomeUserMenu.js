8b0bc38a3d4900b5cfa329c8a33492ebc73650a5
rene_mbp <harryloveslearning@googlemail.com>
2018-02-17 23:58:50 +0100

    fixed lint

--- src/components/HomeUserMenu.js
+++ src/components/HomeUserMenu.js
@@ -2,7 +2,6 @@
 import React, { Component } from 'react'
 import { withRouter }  from 'react-router-dom';
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 
 import Button from 'material-ui/Button'
 
